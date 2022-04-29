8a74a7c0698ffc5ffa43b247af21baaee3ec6b97
mefengl <71683364+mefengl@users.noreply.github.com>
2022-04-29 20:47:36 +0800

    重构:提取函数

--- routes/search/book.js
+++ routes/search/book.js
@@ -3,28 +3,34 @@ const express = require("express")
 const axios = require('axios')
 const router = express.Router()
 
+
 router.get("/:searchValue/:pageIndex", (req, res) => {
   const searchValue = req.params.searchValue
   const pageIndex = req.params.pageIndex
   axios({
-	"method": "POST",
-	"url": "http://opac.ahu.edu.cn/Mobile/jdjs",
-	"headers": {
-		"Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
-	},
-	"data": `str=${searchValue}&pageidx=${pageIndex}`
-})
+    "method": "POST",
+    "url": "http://opac.ahu.edu.cn/Mobile/jdjs",
+    "headers": {
+      "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
+    },
+    "data": `str=${searchValue}&pageidx=${pageIndex}`
+  })
     .then(value => value.data)
-    .then(data => data.map(item=>{
-  const dict = {}
-  dict.name = item.C200A
-  dict.author = item.C200F
-  dict.year = item.C210D
-  dict.publish = item.C210C
-  dict.id = item.REFCODE
-  return dict
-}))
-    .then(value=>res.json(value))
+    .then(data => usefulbookinfo(data))
+    .then(value => res.json(value))
+
+  function usefulbookinfo(data) {
+    return data.map(item => {
+      const dict = {}
+      dict.name = item.C200A
+      dict.author = item.C200F
+      dict.year = item.C210D
+      dict.publish = item.C210C
+      dict.id = item.REFCODE
+      return dict
+    })
+  }
 })
-           
+
 module.exports = router
+
